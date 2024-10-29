6993fd30fc2ba999ff093032acbd0a3cc0cc8441
hyunsuk01 <76142195+hyunsuk01@users.noreply.github.com>
2024-10-29 13:05:20 +0900

    Run 기능 추가 및 상태변환 추가

--- boy.py
+++ boy.py
@@ -1,23 +1,35 @@
 from pico2d import load_image, get_time
-from state_machine import StateMachine, time_out, space_down
+from state_machine import StateMachine, time_out, space_down, right_down, left_down, left_up, right_up, start_event
 
 
 # 상태를 클래스를 통해서 정의함
 class Idle:
     @staticmethod
-    def enter(boy):
-        # 현재 시간을 저장
+    def enter(boy, e):
+        if left_up(e) or right_down(e):
+            boy.action = 2
+            boy.face_dir = -1
+        elif right_up(e) or left_down(e) or start_event(e):
+            boy.action = 3
+            boy.face_dir = 1
+
+        boy.frame = 0
+        boy.dir = 0
+
+        # 시작 시간을 저장
         boy.start_time = get_time()
         pass
+
     @staticmethod
-    def exit(boy):
+    def exit(boy, e):
         pass
+
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
-        if get_time() - boy.start_time > 3:
+        if get_time() - boy.start_time > 1:
+            #이벤트 발생
             boy.state_machine.add_event(('TIME_OUT',0))
-
 
     @staticmethod
     def draw(boy):
@@ -26,23 +38,60 @@ class Idle:
 
 class Sleep:
     @staticmethod
-    def enter(boy):
+    def enter(boy, e):
         pass
+
     @staticmethod
-    def exit(boy):
+    def exit(boy, e):
         pass
+
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
 
     @staticmethod
     def draw(boy):
-        boy.image.clip_composite_draw(
-            boy.frame*100, 300, 100, 100,
-            3.141592/2, # 90도 회전
-            '', # 좌우 상하 반전 x
-            boy.x - 25, boy.y - 25, 100, 100
+        if boy.face_dir == 1: # 오른쪽 방향을 보고 있는 상태
+            boy.image.clip_composite_draw(
+                boy.frame*100, 300, 100, 100,
+                3.141592/2, # 90도 회전
+                '', # 좌우 상하 반전 x
+                boy.x - 25, boy.y - 25, 100, 100
+            )
+        elif boy.face_dir == -1:
+            boy.image.clip_composite_draw(
+                boy.frame*100, 200, 100, 100,
+                3.141592/2*3, '',
+                boy.x + 25, boy.y - 25, 100, 100
+            )
+
+class Run:
+    @staticmethod
+    def enter(boy, e):
+        if right_down(e) or left_up(e):
+            boy.action = 1
+            boy.dir = 1
+        elif left_down(e) or right_up(e):
+            boy.action = 0
+            boy.dir = -1
+        boy.frame = 0
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += boy.dir * 3
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(
+            boy.frame*100, boy.action*100, 100, 100,
+            boy.x, boy.y
         )
+
 
 class Boy:
     def __init__(self):
@@ -52,11 +101,12 @@ class Boy:
         self.action = 3
         self.image = load_image('animation_sheet.png')
         self.state_machine = StateMachine(self) #소년 객체의 state machine 생성
-        self.state_machine.start(Sleep) # 초기 상태가 Idle
+        self.state_machine.start(Idle) # 초기 상태가 Idle
         self.state_machine.set_transitions(
             {
-                Idle : { time_out : Sleep },
-                Sleep : { space_down : Idle }
+                Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
+                Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+                Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle}
             }
         )
 
@@ -74,3 +124,4 @@ class Boy:
     def draw(self):
         self.state_machine.draw()
         # self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+
--- state_machine.py
+++ state_machine.py
@@ -1,7 +1,9 @@
 # 이벤트 체크 함수를 정의
 # 상태 이벤트 e = (종류, 실제값) 튜플로 정의
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
 
+def start_event(e):
+    return e[0] == 'START'
 
 def space_down(e): # e가 space down인지 판단? True of False
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
@@ -9,17 +11,30 @@ def space_down(e): # e가 space down인지 판단? True of False
 def time_out(e): # e가 time out인지 판단?
     return e[0] == 'TIME_OUT'
 
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
 
 class StateMachine:
     def __init__(self, obj):
         self.obj = obj # 어떤 객체를 위한 상태머신인지 알려줌 obj = boy.self
         # 상태 이벤트를 보관할 리스트
         self.event_q = []
-        pass
 
     def start(self, state):
         self.cur_state = state # 시작 상태를 받아서, 그걸로 현재 상태를 정의
-        pass
+        # 새로운 상태로 시작했기 때문에, enter 를 실행해야 한다
+        self.cur_state.enter(self.obj, ('START', 0))
+        print(f'ENTER into {self.cur_state}')
 
     def update(self):
         self.cur_state.do(self.obj) # Idle.do()
@@ -33,19 +48,20 @@ class StateMachine:
             # 다음 상태를 결정하는 방법은?
             # 상태 변환 테이블을 이용.
             for check_event, next_state in self.transitions[self.cur_state].items():
-                if check_event(e): #
-                    self.cur_state.exit(self.obj)
+                if check_event(e):
+                    self.cur_state.exit(self.obj, e)
+                    print(f'EXIT from {self.cur_state}')
                     self.cur_state = next_state
-                    self.cur_state.enter(self.obj)
-        pass
+                    self.cur_state.enter(self.obj, e)
+                    print(f'ENTER into {next_state}')
+                    return
 
     def draw(self):
         self.cur_state.draw(self.obj)
-        pass
 
     def add_event(self, e):
         self.event_q.append(e)
-        pass
+        print(f'    DEBUG: new event {e} is added.')
 
     def set_transitions(self, transitions):
         self.transitions = transitions
